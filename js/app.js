f006262524565b221d6fb7c07d8df0122433b9c8
jgarrison1204 <jgarrison1204@gmail.com>
2017-03-21 17:46:16 -0700

    Added comments

--- js/app.js
+++ js/app.js
@@ -5,20 +5,20 @@ var sections = ["prerequites", "coreRequirements", "specializationRequirements"]
 // Template for all courses that are rendered
 var courseTemplate = "<section id="+"course-wrapper"+" class="+'row'+"><span class="+'col-sm-1'+"><i></i></span><article class="+'col-sm-4'+"><input class="+'course-id'+"></article><div class="+'col-sm-2'+"><span class="+'units'+"></span></div><article class="+'col-sm-2'+"><input class="+'grade'+"></article><div class="+'col-sm-2'+"><span class="+'grade-units'+"></span></section>";
 
-// called on click of any of the group button options
+// called on when button from dropdown list is selected.
 function render(){
 	// pipes in text content from the dropdown-button group of selected button into specialization span.
 	let specializationTitle = $('input[id='+data+']')[0].value;
 	$('#specialization').html(specializationTitle);
-	// loop through sections and renders courses to page by appending courseTemplate to correct section based on length of array from json data.js file.
+	// loop through sections and renders courses to page by appending courseTemplate to correct section based on length of array from the data.js file.
 	$.each(sections, function(indexSections, elementSections){
-		// Returns the courses and GPA course units to render on click. Data is the variable with a value returned from session storage variable filter. elementSections pipes in the section from the section array.
+		// Returns the courses and GPA course units to render on click. Data is the variable with a value returned from session storage variable filter. elementSections pipes in the section from the sections array.
 		let optionToRender = semesterCourses[data][0][elementSections];
 		for (let i = 0; i < optionToRender.length; i++) {
 		    if (elementSections === sections[0]) {
 		    	$("#prerequite").append(courseTemplate);
-		    	// Adds class with the name of the section to be able to filter for Quater Courses on focus. Repeats for all inputs with 'course-id'
-		    	$('input.course-id').addClass(sections[0]);
+		    	// Adds class with the name of the section to be able to filter for Quater Courses on focus. Repeats for all inputs with 'course-id' and bootstrap cols for responsive design.
+  		    	$('input.course-id').addClass(sections[0]);
 		    	$('article.col-sm-4').addClass('col-xs-7');
 		    	$('article.col-sm-2').addClass('col-xs-4');
 		    } else if (elementSections === sections[1]) {
@@ -37,30 +37,30 @@ function render(){
 		    $("section span i").parent().addClass("col-xs-1");
 		}
 	});
-	// New array with elements copied from the nodes array returned from querySelectorAll so we can envoke array methods on items.
-	var nodesArray = [].slice.call(document.querySelectorAll('input.course-id'));
-	//Return values of semester courses from selected option (button click or value of data) to extract correct courses and units. 
-	let courseNumber = Object.values(semesterCourses[data][0]);
-	//hold all courses for selected option.
-	var coursesByOption = [];
-	//hold all GPAUnits for selected option.
-	var unitsByOption = [];
-	// push all courses and units into coursesByOption and  untisByOPtions Array.
-	courseNumber.forEach(function(e, i) {
-		e.forEach(function(nestArryElement, nestArryIndex) {
-			coursesByOption.push(nestArryElement.value);
-			unitsByOption.push(nestArryElement.data);
-		});
-	});
-	// 
-	$.each(nodesArray, function(i,e) {
-		// render the course into the input
-		$(e).val(coursesByOption[i]);
-		// render the GPA units value into the input
-		$(this).parent().next().children().html(unitsByOption[i]);
-	});
 	// Document ready being called as code below is determined by user input.
 	$(document).ready(function(){
+		// New array with elements copied from the nodes array returned from querySelectorAll so we can envoke array methods on items.
+		var coursesArray = [].slice.call(document.querySelectorAll('input.course-id'));
+		//Return values of semester courses from selected option (button click or value of data) to extract correct courses and units. 
+		let courseNumber = Object.values(semesterCourses[data][0]);
+		//hold all courses for selected option.
+		var coursesByOption = [];
+		//hold all GPAUnits for selected option.
+		var unitsByOption = [];
+		// push all courses and units into coursesByOption and  untisByOPtions Array.
+		courseNumber.forEach(function(e, i) {
+			e.forEach(function(nestArryElement, nestArryIndex) {
+				coursesByOption.push(nestArryElement.value);
+				unitsByOption.push(nestArryElement.data);
+			});
+		});
+		// 
+		$.each(coursesArray, function(i,e) {
+			// render the course into the input
+			$(e).val(coursesByOption[i]);
+			// render the GPA units value into the input
+			$(this).parent().next().children().html(unitsByOption[i]);
+		});
 		// When user clicks in an input.....
 	    $('input').focus(function(){
 	    	// to change the course that was rendered... 
@@ -82,7 +82,7 @@ function render(){
 				    lookup: grades,
 				    onSelect: function (suggestion) {
 						$(this).parent().next().children().html(suggestion.data);
-						// Explicit coerion from sting to number.
+						// Sets the GPA Units and the grades to calculate GPA after grade is entered.
 						let gpaPointsForCalc = $(this).parent().next().children()[0];
 						let courseIdForCalc = $(this).parent().siblings().find('span.units')[0];
 						sumUnits(gpaPointsForCalc, courseIdForCalc);
@@ -91,7 +91,7 @@ function render(){
 			}
 		});
 		$('input').focus(function(){
-			let ogValue = $(this).val();
+			// if input is a grade input and it has a value do.....
 			if ($(this).hasClass('grade') && $(this).val()) {
 				$(this).blur(function(){
 					if (!$(this).val()) {
@@ -142,11 +142,25 @@ $("input[name=options]").focus(function(){
 // re-render the page with data now set to the value of clicked button.
 render();
 
+// Addes identical ids to spans with units or grade-units. 
+var nodesArray = [].slice.call(document.querySelectorAll('span.units'));
+nodesArray.forEach(function(e,i) {
+	let counterId = i;
+	counterId++;
+	$(e).attr('id', counterId);
+})
+var gradesArray = [].slice.call(document.querySelectorAll('span.grade-units'));
+gradesArray.forEach(function(e,i) {
+	let counterId = i;
+	counterId++;
+	$(e).attr('id', counterId);
+})
+
 // Empty arrays that will return entered grade values and untis for calculation.
 let unitsObj = {};
 let gradesObj = {};
 
-// Invoked everytime user enters a grade. Only renders final GPA once all grades have been enter.  Will refactor sunUnits, gpaCalc if needed. Code works as follows: returns 2 arrays of (1) all GPA units and (2) grades entered.  GPA units is filled on render. The gradesAry is filled one value at a time by user entering grade. Patterned this way because oringially though of adding courses one by one with click button.
+// Invoked everytime user enters a grade. Renders GPA after each grade has been entered or deleted.  Will refactor sunUnits, gpaCalc if needed. Code works as follows: returns 2 arrays of (1) all GPA units and (2) grades entered.  GPA units is filled on render. The gradesAry is filled one value at a time by user entering grade. Patterned this way because oringially though of adding courses one by one with click button.
 function sumUnits (inputGrade, inputCourse) {
 	unitsObj[inputCourse.id] = +inputCourse.textContent;
 	gradesObj[inputGrade.id] = +inputGrade.textContent;
@@ -179,18 +193,3 @@ function gpaCalc(units, grades) {
 		$('#prerequite').hide();
 	}
 })();
-
-
-var nodesArray = [].slice.call(document.querySelectorAll('span.units'));
-nodesArray.forEach(function(e,i) {
-	let counterId = i;
-	counterId++;
-	$(e).attr('id', counterId);
-})
-var gradesArray = [].slice.call(document.querySelectorAll('span.grade-units'));
-gradesArray.forEach(function(e,i) {
-	let counterId = i;
-	counterId++;
-	$(e).attr('id', counterId);
-})
-
